fc44204cfc24329d5c33543da94016bfcaeb95f2
Soner Yuksel <grkmyksl@gmail.com>
2021-04-16 14:35:44 -0400

    Adding History Node and History Api Template

--- ios/browser/api/history/brave_history_api.h
+++ ios/browser/api/history/brave_history_api.h
@@ -1,31 +1,60 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+ 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-// Public Interface for History Object to be used iOS
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSUInteger, HistoryNodeFaviconState) {
+  HistoryNodeFaviconStateInvalidFavIcon,
+  HistoryNodeFaviconStateLoadingFavIcon,
+  HistoryNodeFaviconStateLoadedFavIcon,
+};
+
+// @protocol HistoryModelObserver;
+// @protocol HistoryModelListener;
+
+@class IOSHistoryNode;
+
+NS_SWIFT_NAME(HistoryNode)
 OBJC_EXPORT
 @interface IOSHistoryNode : NSObject
 
-// Title and URL for the History object
 @property (nonatomic, strong, readonly) NSString *title;
-@property (nonatomic, strong, readonly) NSURL *url;
+@property(nonatomic, copy, readonly) NSString* guid;
+@property(nonatomic, nullable, copy) NSURL* url;
 
-// The Date properties
-@property (nonatomic, strong, readonly) NSDate *dateAdded;
-@property (nonatomic, strong, readonly) NSDate *lastVisitedDate;
+@property(nonatomic, nullable, copy, readonly) UIImage* icon;
+@property(nonatomic, nullable, copy, readonly) NSURL* iconUrl;
 
-// Fav Icon related properties
-@property (nonatomic, strong, readonly) UIImage *favIcon;
-@property (nonatomic, strong, readonly) NSURL *favIconURL;
+@property(nonatomic, copy) NSDate* dateAdded;
+@property(nonatomic, copy) NSDate* lastVisitedDate;
 
-@property (nonatomic, strong, readonly) UIColor *skcolor;
+- (void)setTitle:(NSString*)title;
 
-// Constructor declaring basic History properties
-- (instancetype) init;
-- (instancetype) initWithTitle:(NSString *)title withURL:(NSURL *)url;
+- (instancetype)initWithTitle:(NSString*)title
+                           id:(int64_t)id
+                         guid:(NSString* _Nullable)guid
+                          url:(NSURL* _Nullable)url
+                    dateAdded:(NSDate* _Nullable)dateAdded;
+@end
+
+NS_SWIFT_NAME(BraveHistoryAPI)
+OBJC_EXPORT
+@interface BraveHistoryAPI : NSObject
+@property(class, readonly, getter = sharedHistoryAPI) BraveHistoryAPI* shared;
+
+@property(nonatomic, readonly) bool isLoaded;
+
+// - (id<BookmarkModelListener>)addObserver:(id<HistoryModelObserver>)observer;
+// - (void)removeObserver:(id<HistoryModelListener>)observer;
+
+- (void)removeHistory:(IOSHistoryNode*)history;
+- (void)removeAll;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- ios/browser/api/history/brave_history_api.mm
+++ ios/browser/api/history/brave_history_api.mm
@@ -1,69 +1,188 @@
-/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <memory>
+#include "brave/ios/browser/api/history/brave_history_api.h"
+
+#include "base/compiler_specific.h"
+#include "base/containers/adapters.h"
+#include "base/guid.h"
 #include "base/strings/sys_string_conversions.h"
+
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_manager.h"
+#include "ios/chrome/browser/application_context.h"
+
+#include "ios/web/public/thread/web_thread.h"
 #import "net/base/mac/url_conversions.h"
 #include "url/gurl.h"
 
-#import "brave/ios/browser/api/history/brave_history_api.h"
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
-#include "components/history/core/browser/top_sites.h"
-#include "components/history/core/browser/history_types.h"
+using namespace base;
 
-#include "third_party/skia/include/core/SkColor.h"
-
-@interface IOSHistoryNode()
-{
-    // history::PrepopulatedPage* _pre_populated_page;
-    std::unique_ptr<history::PrepopulatedPage> _pre_populated_page;
+@interface IOSHistoryNode () {
+  bool owned_;
 }
 @end
 
-
 @implementation IOSHistoryNode
 
-- (instancetype)init {
-    if ((self = [super init])) {
-        //CHECK(false);
+- (instancetype)initWithTitle:(NSString*)title
+                           id:(int64_t)id
+                         guid:(NSString*)guid
+                          url:(NSURL*)url
+                    dateAdded:(NSDate*)dateAdded {
+  if ((self = [super init])) {
+    // Title
+    string16 title_ = SysNSStringToUTF16(title);
+
+    // TID
+    int64_t id_ = static_cast<int64_t>(id);
+    DCHECK(id_);
+
+    // UID
+    GUID guid_ = GUID();
+    if ([guid length] > 0) {
+      string16 guid_string = SysNSStringToUTF16(guid);
+      DCHECK(IsValidGUID(guid_string));
+      guid_ = GUID::ParseCaseInsensitive(guid_string);
+    } else {
+      guid_ = GUID::GenerateRandomV4();
     }
-    return self;
+
+    // URL
+    GURL gurl_ = net::GURLWithNSURL(url);
+
+    // Date Added
+    if (dateAdded) {
+      // base::Time date_added_ = base::Time::FromDoubleT([dateAdded timeIntervalSince1970]);
+    }
+
+    owned_ = true;
+  }
+
+  return self;
 }
 
-- (instancetype) initWithTitle:(NSString *)title withURL:(NSURL *)url {
-    if ((self = [super init])) {
-        // _pre_populated_page = new PrepopulatedPage()
+- (void)dealloc {
+  if (owned_) {
+    owned_ = false;
+  }
+}
+
+- (void)setTitle:(NSString*)title {
+
+}
+
+- (NSString*)title {
+  NSString *title = @"History Title";
+  return title;
+}
+
+- (NSString*)guid {
+  NSString *guid = @"GUID";
+  return guid;
+}
+
+- (void)setUrl:(NSURL*)url {
     
-        // _pre_populated_page = std::unique_ptr<PrepopulatedPage>(
-        //     url,
-        //     title,
-        //     favicon_id,
-        //     color            
-        // )
-
-        // Converting GURL to NSURL
-        // GURL gurl_ = net::GURLWithNSURL(url);
-
-        // // Converting base::string to NSString
-        // base::string16 gTitle_ = base::SysNSStringToUTF16(title);
-
-        // _pre_populated_page = std::make_unique(
-        //     gurl_,
-        //     gTitle_,
-        //     0 //favicon_id
-        //     SkColor //color
-        // )
-    }
-    return self;
 }
 
-// - (NSString *)title {
-//     return base::SysNSStringToUTF16(_pre_populated_page->title());
+- (NSURL*)url {
+  NSString *historyURLString = @"www.brave.com";
+  NSURL *testURL = [NSURL URLWithString: historyURLString];
+
+  return testURL;
+}
+
+- (NSURL*)iconUrl {
+  NSString *iconURLString = @"www.brave.com";
+  NSURL *testIconURL = [NSURL URLWithString: iconURLString];
+
+  return testIconURL;
+}
+
+- (UIImage*)icon {
+  UIImage *testIconImage = [UIImage imageNamed:@"test_img"];
+  return testIconImage;
+}
+
+- (NSDate*)dateAdded {
+  NSDate *testDate= [NSDate date];
+  return testDate;
+}
+
+- (void)setDateAdded:(NSDate*)date {
+
+}
+
+- (bool)isFavIconLoaded {
+  return true;
+}
+
+- (bool)isFavIconLoading {
+  return true;
+}
+
+- (void)remove {
+
+}
+
+@end
+
+@interface BraveHistoryAPI ()  {
+}
+@end
+
+@implementation BraveHistoryAPI
++ (instancetype)sharedHistoryAPI {
+  static BraveHistoryAPI* instance = nil;
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    instance = [[BraveHistoryAPI alloc] init];
+  });
+  return instance;
+}
+
+- (instancetype)init {
+  if ((self = [super init])) {
+    DCHECK_CURRENTLY_ON(web::WebThread::UI);
+    ios::ChromeBrowserStateManager* browserStateManager =
+        GetApplicationContext()->GetChromeBrowserStateManager();
+    ChromeBrowserState* browserState =
+        browserStateManager->GetLastUsedBrowserState();
+
+    DCHECK(browserState);
+  }
+  return self;
+}
+
+- (void)dealloc {
+}
+
+- (bool)isLoaded {
+    return true;
+}
+
+// - (id<HistoryModelListener>)addObserver:(id<HistoryModelObserver>)observer {
+// //   return [[HistoryModelListenerImpl alloc] init:observer
+// //                                    historyModel:history_model_];
 // }
 
-// - (void)dealloc {
-//     delete _pre_populated_page
+// - (void)removeObserver:(id<HistoryModelListener>)observer {
+// //   [observer destroy];
 // }
+
+- (void)removeHistory:(IOSHistoryNode*)history {
+  DCHECK_CURRENTLY_ON(web::WebThread::UI);
+}
+
+- (void)removeAll {
+  DCHECK_CURRENTLY_ON(web::WebThread::UI);
+}
+
 @end
