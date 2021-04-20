5677693b8320726933a332fa8c1c51b0c78faf6d
Mario Sanchez Prada <mario@igalia.com>
2021-04-20 12:49:39 +0200

    Migrate non-tests usage of base::ASCIIToUTF16("...") to u"..."
    We need to use either u"..." literals or char16_t constant strings now.
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src.git/+/522370fb5f2b8ec0719a3752bcac4897164dd32d
    
    commit 522370fb5f2b8ec0719a3752bcac4897164dd32d
    Author: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Date:   Fri Apr 16 17:22:39 2021 +0000
    
        [LSC] Disallow base::ASCIIToUTF16("...") outside of tests
    
        This change modifies base::ASCIIToUTF16 to cause compilation errors when
        it is used with a string constant outside of tests. Instead, callers
        should just use a UTF16 literal (u"...") instead.
    
        Bug: 1189439
    
    fixup! Migrate non-tests usage of base::ASCIIToUTF16("...") to u"..."

--- chromium_src/chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chromium_src/chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -47,21 +47,22 @@ void BraveAddLocalizedStrings(content::WebUIDataSource*, Profile*);
 #include "brave/browser/ui/webui/brave_settings_ui.h"
 namespace settings {
 
-const char kWebRTCLearnMoreURL[] =
-    "https://support.brave.com/hc/en-us/articles/"
-    "360017989132-How-do-I-change-my-Privacy-Settings-#webrtc";
-const char kBraveBuildInstructionsUrl[] =
-    "https://github.com/brave/brave-browser/wiki";
-const char kBraveLicenseUrl[] = "https://mozilla.org/MPL/2.0/";
-const char kBraveReleaseTagPrefix[] =
-    "https://github.com/brave/brave-browser/releases/tag/v";
-const char kGoogleLoginLearnMoreURL[] =
-    "https://github.com/brave/brave-browser/wiki/"
-    "Allow-Google-login---Third-Parties-and-Extensions";
-const char kDNSLinkLearnMoreURL[] = "https://docs.ipfs.io/concepts/dnslink/";
-const char kUnstoppableDomainsLearnMoreURL[] =
-    "https://github.com/brave/brave-browser/wiki/"
-    "Resolve-Methods-for-Unstoppable-Domains";
+const char16_t kWebRTCLearnMoreURL[] =
+    u"https://support.brave.com/hc/en-us/articles/"
+    u"360017989132-How-do-I-change-my-Privacy-Settings-#webrtc";
+const char16_t kBraveBuildInstructionsUrl[] =
+    u"https://github.com/brave/brave-browser/wiki";
+const char16_t kBraveLicenseUrl[] = u"https://mozilla.org/MPL/2.0/";
+const char16_t kBraveReleaseTagPrefix[] =
+    u"https://github.com/brave/brave-browser/releases/tag/v";
+const char16_t kGoogleLoginLearnMoreURL[] =
+    u"https://github.com/brave/brave-browser/wiki/"
+    u"Allow-Google-login---Third-Parties-and-Extensions";
+const char16_t kDNSLinkLearnMoreURL[] =
+    u"https://docs.ipfs.io/concepts/dnslink/";
+const char16_t kUnstoppableDomainsLearnMoreURL[] =
+    u"https://github.com/brave/brave-browser/wiki/"
+    u"Resolve-Methods-for-Unstoppable-Domains";
 
 void BraveAddCommonStrings(content::WebUIDataSource* html_source,
                            Profile* profile) {
@@ -273,12 +274,9 @@ void BraveAddCommonStrings(content::WebUIDataSource* html_source,
     {"showFullUrls", IDS_CONTEXT_MENU_SHOW_FULL_URLS},
   };
   html_source->AddLocalizedStrings(localized_strings);
-  html_source->AddString("webRTCLearnMoreURL",
-                         base::ASCIIToUTF16(kWebRTCLearnMoreURL));
-  html_source->AddString("googleLoginLearnMoreURL",
-                         base::ASCIIToUTF16(kGoogleLoginLearnMoreURL));
-  html_source->AddString("ipfsDNSLinkLearnMoreURL",
-                         base::UTF8ToUTF16(kDNSLinkLearnMoreURL));
+  html_source->AddString("webRTCLearnMoreURL", kWebRTCLearnMoreURL);
+  html_source->AddString("googleLoginLearnMoreURL", kGoogleLoginLearnMoreURL);
+  html_source->AddString("ipfsDNSLinkLearnMoreURL", kDNSLinkLearnMoreURL);
   html_source->AddString(
       "getMoreExtensionsUrl",
       base::ASCIIToUTF16(
@@ -295,11 +293,10 @@ void BraveAddCommonStrings(content::WebUIDataSource* html_source,
                                  base::ASCIIToUTF16(ipfs::kIPFSLearnMoreURL));
   html_source->AddString("ipfsMethodDesc", ipfs_method_desc);
 
-  html_source->AddString(
-      "resolveUnstoppableDomainsSubDesc",
-      l10n_util::GetStringFUTF16(
-          IDS_SETTINGS_RESOLVE_UNSTOPPABLE_DOMAINS_SUB_DESC,
-          base::ASCIIToUTF16(kUnstoppableDomainsLearnMoreURL)));
+  html_source->AddString("resolveUnstoppableDomainsSubDesc",
+                         l10n_util::GetStringFUTF16(
+                             IDS_SETTINGS_RESOLVE_UNSTOPPABLE_DOMAINS_SUB_DESC,
+                             kUnstoppableDomainsLearnMoreURL));
 }
 
 void BraveAddResources(content::WebUIDataSource* html_source,
@@ -310,10 +307,10 @@ void BraveAddResources(content::WebUIDataSource* html_source,
 void BraveAddAboutStrings(content::WebUIDataSource* html_source,
                           Profile* profile) {
   std::u16string license = l10n_util::GetStringFUTF16(
-      IDS_BRAVE_VERSION_UI_LICENSE, base::ASCIIToUTF16(kBraveLicenseUrl),
+      IDS_BRAVE_VERSION_UI_LICENSE, kBraveLicenseUrl,
       base::ASCIIToUTF16(chrome::kChromeUICreditsURL),
-      base::ASCIIToUTF16(kBraveBuildInstructionsUrl),
-      base::ASCIIToUTF16(kBraveReleaseTagPrefix) +
+      kBraveBuildInstructionsUrl,
+      kBraveReleaseTagPrefix +
           base::UTF8ToUTF16(
               version_info::GetBraveVersionWithoutChromiumMajorVersion()));
   html_source->AddString("aboutProductLicense", license);
