146e2278f13c8c3a63d5def135227eae74b9c40d
Sergey P <spylogsster@gmail.com>
2021-04-26 11:55:09 +0300

    the page is laoded

--- browser/resources/settings/brave_ipfs_page/p2p_keys_subpage.js
+++ browser/resources/settings/brave_ipfs_page/p2p_keys_subpage.js
@@ -7,7 +7,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
-
 /**
  * @fileoverview
  * 'brave-sync-setup' is the UI for starting or joining a sync chain
