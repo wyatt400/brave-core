60affe196ac50d02698be893dce7b097e2dd83db
Sergey P <spylogsster@gmail.com>
2021-04-26 11:24:08 +0300

    fix

--- browser/resources/settings/brave_ipfs_page/p2p_keys_subpage.js
+++ browser/resources/settings/brave_ipfs_page/p2p_keys_subpage.js
@@ -13,19 +13,16 @@
  * 'brave-sync-setup' is the UI for starting or joining a sync chain
  * settings.
  */
-import {Polymer, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {Route, RouteObserverBehavior, Router} from '../router.m.js';
+import {Router} from '../router.m.js';
+ 
 /**
 * @fileoverview
 * 'settings-sync-subpage' is the settings page content
 */
 Polymer({
-  is: 'settings-p2p-keys-manager',
-
-  _template: html`{__html_template__}`,
+  is: 'settings-p2p-keys-subpage',
 
   behaviors: [
-    RouteObserverBehavior,
   ],
 
   properties: {
@@ -44,144 +41,22 @@ Polymer({
   /** @private {?SyncBrowserProxy} */
   browserProxy_: null,
 
-  /**
-  * The beforeunload callback is used to show the 'Leave site' dialog. This
-  * makes sure that the user has the chance to go back and confirm the sync
-  * opt-in before leaving.
-  *
-  * This property is non-null if the user is currently navigated on the sync
-  * settings route.
-  *
-  * @private {?Function}
-  */
-  beforeunloadCallback_: null,
-
-  /**
-  * The unload callback is used to cancel the sync setup when the user hits
-  * the browser back button after arriving on the page.
-  * Note: Cases like closing the tab or reloading don't need to be handled,
-  * because they are already caught in |PeopleHandler::~PeopleHandler|
-  * from the C++ code.
-  *
-  * @private {?Function}
-  */
-  unloadCallback_: null,
-
-  /**
-  * Whether the user completed setup successfully.
-  * @private {boolean}
-  */
-  setupSuccessful_: false,
 
   /** @override */
   created: function() {
-    console.log(11);
+    console.log("created");
     // this.browserProxy_ = SyncBrowserProxyImpl.getInstance();
   },
-
+  /** @override */
+  ready: function() {
+    console.log("ready");
+  },
   /** @override */
   attached: function() {
-    console.log(11);
+    console.log("attached");
     const router = Router.getInstance();
     if (router.getCurrentRoute() == router.getRoutes().BRAVE_IPFS_KEYS) {
       this.onNavigateToPage_();
-    }
-  },
-
-  /** @override */
-  detached: function() {
-    const router = Router.getInstance();
-    if (router.getRoutes().BRAVE_IPFS_KEYS.contains(router.getCurrentRoute())) {
-      this.onNavigateAwayFromPage_();
-    }
-
-    if (this.beforeunloadCallback_) {
-      window.removeEventListener('beforeunload', this.beforeunloadCallback_);
-      this.beforeunloadCallback_ = null;
-    }
-    if (this.unloadCallback_) {
-      window.removeEventListener('unload', this.unloadCallback_);
-      this.unloadCallback_ = null;
-    }
-  },
-
-  updatePageStatus_: function () {
-    //const isFirstSetup = this.syncStatus && this.syncStatus.firstSetupInProgress
-    //this.pageStatus_ = isFirstSetup ? 'setup' : 'configure'
-  },
-
-  /** @protected */
-  currentRouteChanged: function() {
-    const router = Router.getInstance();
-    if (router.getCurrentRoute() == router.getRoutes().BRAVE_IPFS_KEYS) {
-      this.onNavigateToPage_();
-      return;
-    }
-
-    if (router.getRoutes().BRAVE_IPFS_KEYS.contains(router.getCurrentRoute())) {
-      return;
-    }
-
-    this.onNavigateAwayFromPage_();
-  },
-
-  /**
-  * @param {!PageStatus} expectedPageStatus
-  * @return {boolean}
-  * @private
-  */
-  isStatus_: function(expectedPageStatus) {
-    return expectedPageStatus == this.pageStatus_;
-  },
-
-  /** @private */
-  onNavigateToPage_: function() {
-    const router = Router.getInstance();
-    assert(router.getCurrentRoute() == router.getRoutes().BRAVE_IPFS_KEYS);
-    if (this.beforeunloadCallback_) {
-      return;
-    }
-
-    // Triggers push of prefs to our handler
-    //if (this.browserProxy_)
-      //this.browserProxy_.didNavigateToSyncPage();
-
-    this.unloadCallback_ = this.onNavigateAwayFromPage_.bind(this);
-    window.addEventListener('unload', this.unloadCallback_);
-  },
-
-  /** @private */
-  onNavigateAwayFromPage_: function() {
-    if (!this.beforeunloadCallback_) {
-      return;
-    }
-
-    this.browserProxy_.didNavigateAwayFromSyncPage(!this.setupSuccessful_);
-
-    // Reset state as this component could actually be kept around even though
-    // it is hidden.
-    this.setupSuccessful_ = false
-
-    window.removeEventListener('beforeunload', this.beforeunloadCallback_);
-    this.beforeunloadCallback_ = null;
-
-    if (this.unloadCallback_) {
-      window.removeEventListener('unload', this.unloadCallback_);
-      this.unloadCallback_ = null;
-    }
-  },
-
-  /**
-  * Called when setup is complete and sync code is set
-  * @private
-  */
-  onSetupSuccess_: function() {
-    this.setupSuccessful_ = true
-    // This navigation causes the firstSetupInProgress flag to be marked as false
-    // via `didNavigateAwayFromSyncPage`.
-    const router = Router.getInstance();
-    if (router.getCurrentRoute() == router.getRoutes().BRAVE_IPFS_KEYS) {
-      router.navigateTo(router.getRoutes().BRAVE_IPFS);
     }
   },
 });
